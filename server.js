4fc5154135834cc71b236e8d7c1d7b9c58576caf
ningyuv <ningyuv@outlook.com>
2018-12-12 15:21:08 +0800

    edit

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ app.get('/sharePage/list', (req, res)=>{
     connection.query(sql, (err, result)=>{
         connection.end()
         if(err) {
-            console.log(err.message)
-            res.end(JSON.stringify(err.message))
+            console.log(err)
+            res.end(JSON.stringify(err))
         }
         res.end(JSON.stringify(result))
     })
@@ -40,8 +40,8 @@ app.get('/sharePage/links/:id', (req, res)=>{
     connection.query(sql, (err, result)=>{
         connection.end()
         if(err) {
-            console.log(err.message)
-            res.end(JSON.stringify(err.message))
+            console.log(err)
+            res.end(JSON.stringify(err))
         }
         res.end(JSON.stringify(result))
     })
@@ -54,8 +54,8 @@ app.post('/sharePage/login', (req, res)=>{
     var sql = "SELECT password,token,logged FROM admins WHERE username = '" + req.body.username + "'"
     connection.query(sql, (err, result)=>{
         if(err) {
-            console.log(err.message)
-            res.end(JSON.stringify(err.message))
+            console.log(err)
+            res.end(JSON.stringify(err))
         }
         if(result.length && result[0].password === md5(req.body.password)){
             if(!result[0].logged){
@@ -63,13 +63,13 @@ app.post('/sharePage/login', (req, res)=>{
                 connection.query(sql, (err)=>{
                     connection.end()
                     if(err) {
-                        console.log(err.message)
+                        console.log(err)
                     }
                     else{
                         scheduleWork(req.body.username)
                         console.log(req.body.username, result[0].token)
                         res.end(JSON.stringify({
-                            canLogin:'yes',
+                            code: 'success',
                             token:result[0].token
                         }));
                     }
@@ -77,15 +77,15 @@ app.post('/sharePage/login', (req, res)=>{
             }
             else{
                 res.end(JSON.stringify({
-                    canLogin:'yes',
+                    code: 'success',
                     token:result[0].token
                 }));
             }
         }
         else{
             res.end(JSON.stringify({
-                canLogin:'no',
-                    token:result[0].token
+                code: 'LOGIN_FAILED',
+                token:result[0].token
             }));
         }
     })
@@ -100,29 +100,142 @@ app.post('/sharePage/logout', (req, res)=>{
     connection.query(sql, (err, result)=>{
         connection.end()
         if(err) {
-            console.log(err.message)
-            res.end(JSON.stringify(err.message))
+            console.log(err)
+            res.end(JSON.stringify(err))
         }
         console.log('logout')
         res.end(JSON.stringify({
-            canLogout:'yes'
+            code: 'success'
         }));
     })
     scheduleDone(req.body.username)
+})
+app.post('/sharePage/article/edit', (req, res)=>{
+    console.log(req.body.id+"+"+req.body.title+"+"+req.body.image+"+"+req.body.content+"+"+req.body.language+
+    "+"+req.body.platform+"+"+req.body.downloadMethod+"+"+req.body.download+"+"+req.body.type)
+    res.setHeader('Content-Type', 'text/json; charset=utf8');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    const connection = mysql.createConnection(config);
+    connection.connect()
+    var sql = "SELECT logged FROM admins WHERE token = '" + req.body.token + "'"
+    connection.query(sql, (err,result)=>{
+        if(err){
+            console.log(err)
+            res.end(JSON.stringify(err))
+        }
+        console.log(result)
+        if(result.length && parseInt(result[0].logged)===1){
+            console.log(typeof(req.body.id))
+            if(parseInt(req.body.id)!==0){
+                sql = "UPDATE articles SET title = '" + req.body.title +
+                    "',image = '" + req.body.image +
+                    "',content = '" + req.body.content +
+                    "',language = '" + req.body.language +
+                    "',platform = '" + req.body.platform +
+                    "',downloadMethod = '" + req.body.downloadMethod +
+                    "',download = '" + req.body.download +
+                    "',type = '" + req.body.type + "' WHERE id = " + req.body.id;
+                connection.query(sql, (err, result)=>{
+                    connection.end()
+                    if(err){
+                        console.log(err)
+                        res.end(JSON.stringify(err))
+                    }
+                    console.log(result)
+                    res.end(JSON.stringify({
+                        code: 'success'
+                    }))
+                })
+            }
+            else{
+                sql = "INSERT INTO articles(title,image,content,language,platform,downloadMethod,download,type)" +
+                    "VALUES('"+req.body.title+"','"+req.body.image+"','"+req.body.content+"','"+req.body.language+
+                    "','"+req.body.platform+"','"+req.body.downloadMethod+"','"+req.body.download+"','"+req.body.type+"')"
+                connection.query(sql, (err, result)=>{
+                    connection.end()
+                    if(err){
+                        console.log(err)
+                        res.end(JSON.stringify(err))
+                    }
+                    console.log(result)
+                    res.end(JSON.stringify({
+                        code: 'success'
+                    }))
+                })
+            }
+        }
+        else{
+            res.end(JSON.stringify({
+                code: 'HAVE_NO_ACCESS'
+            }))
+        }
+    })
+})
+app.post('/sharePage/link/edit', (req, res)=>{
+    res.setHeader('Content-Type', 'text/json; charset=utf8');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    const connection = mysql.createConnection(config);
+    connection.connect()
+    var sql = "SELECT logged FROM admins WHERE token = '" + req.body.token + "'"
+    connection.query(sql, (err,result)=>{
+        if(err){
+            console.log(err)
+            res.end(JSON.stringify(err))
+        }
+        console.log(result)
+        if(result.length && parseInt(result[0].logged)===1){
+            if(parseInt(req.body.id)!==0){
+                sql = "UPDATE links SET description = '" + req.body.description +
+                    "',link = '" + req.body.link +
+                    "',code = '" + req.body.code +
+                    "',article_id = '" + req.body.article_id + "' WHERE id = " + req.body.id;
+                connection.query(sql, (err, result)=>{
+                    connection.end()
+                    if(err){
+                        console.log(err)
+                        res.end(JSON.stringify(err))
+                    }
+                    console.log(result)
+                    res.end(JSON.stringify({
+                        code: 'success'
+                    }))
+                })
+            }
+            else{
+                sql = "INSERT INTO links(description,link,code,article_id)" +
+                    "VALUES('"+req.body.description+"','"+req.body.link+"','"+req.body.code+"','"+req.body.article_id+"')"
+                connection.query(sql, (err, result)=>{
+                    connection.end()
+                    if(err){
+                        console.log(err)
+                        res.end(JSON.stringify(err))
+                    }
+                    console.log(result)
+                    res.end(JSON.stringify({
+                        code: 'success'
+                    }))
+                })
+            }
+        }
+        else{
+            res.end(JSON.stringify({
+                code: 'HAVE_NO_ACCESS'
+            }))
+        }
+    })
 })
 
 var server = app.listen(8082, function () {
     var host = server.address().address
     var port = server.address().port
     console.log("应用实例，访问地址为 http://%s:%s", host, port)
-
 })
 const scheduleWork = (username)=>{
     const connection = mysql.createConnection(config);
     connection.connect()
     schedules[username] = schedule.scheduleJob('0 0 0 * * *',()=>{
         var token = stringRandom(32);
-        var sql = "UPDATE admins SET token = '" + token + "' WHERE username = '" + username + "'"
+        var sql = "UPDATE admins SET token = '" + token + "', logged = false WHERE username = '" + username + "'"
         connection.query(sql, ()=>{
             connection.end()
         })
